81fb5d1729fce4006effccb9995f3cd1b4e58cdf
george <george.moore@cyted.ai>
2025-04-24 18:36:46 +0100

    add elastic IP address for the NAT gateway

--- terraform/modules/aws_nat_gateway/main.tf
+++ terraform/modules/aws_nat_gateway/main.tf
@@ -1,4 +1,9 @@
 
+resource "aws_eip" "this" {
+  domain = "vpc"
+}
+
 resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.this.id // Public NAT gateways require an elastic IP address.
   subnet_id = var.subnet_id
 }
